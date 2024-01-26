7aa0522a219c6bd8c194e0d2821a8143832eea0d
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2024-01-26 17:17:56 +0100

    initial commit

---
+++ src/components/CVSection.js
@@ -0,0 +1,16 @@
+// src/components/CVSection.js
+
+import React from 'react';
+import '../styles/CVSection.scss';
+
+const CVSection = () => {
+  return (
+    <section className="cv-section">
+      <h2>Curriculum Vitae</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis massa in erat fermentum, id blandit mi scelerisque.</p>
+      {/* Dodaj więcej treści CV w miarę potrzeb */}
+    </section>
+  );
+}
+
+export default CVSection;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// src/components/Header.js
+
+import React from 'react';
+import '../styles/Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>NewITstar</h1>
+      <p>Your Journey to IT Excellence</p>
+    </header>
+  );
+}
+
+export default Header;
